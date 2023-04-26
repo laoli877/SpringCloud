2033928d74c4ffb41e3ff47c3f91e91adaa4281b
阿尔法 <laoli877@gmail.com>
2023-04-26 17:48:43 +0800

    修改

--- src/main/java/com/example/SpringBootWebTestApplication.java
+++ src/main/java/com/example/SpringBootWebTestApplication.java
@@ -10,6 +10,7 @@ import java.util.*;
 public class SpringBootWebTestApplication {
 
     public static void main(String[] args) {
+        //test
         SpringApplication.run(SpringBootWebTestApplication.class, args);
     }
 
